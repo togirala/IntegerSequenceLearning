3eaafb740bba0b2d1d2941aa89ead882708b0c56
Tejaswi Ogirala <tejaswi.ogirala@sap.com>
2016-09-18 20:52:30 -0700

    Feature - Difference
    Trying to create a freture with difference b/w 2 consecutive integers

--- src/test.py
+++ src/test.py
@@ -10,14 +10,40 @@ testData = pd.read_csv('C:\Users\I854540\WorkSpace\IntegerSequenceLearning\data\
 
 # print count
 
+testData['difference'] = ''
+def createDifferenceFeature(arraySize):
+	difference = np.zeros(arraySize-1)
+	return difference
 
+
+# def convertDefference(numpyArray):
+
+# 	return concatedString
+
+# difference = np.zeros(range(len(testData)))
 for eachRow in range(len(testData)):
 	completeString = testData.Sequence[eachRow]
 	splitString = completeString.split(',')
+	difference = createDifferenceFeature(len(splitString))
+	# print len(splitString)
 	for eachElement in range(len(splitString)-1):
-		difference = int(splitString[eachElement+1]) - int(splitString[eachElement])
-		
-print(splitString)
+		difference[eachElement] = round(int(splitString[eachElement+1]) - int(splitString[eachElement]))
+	print difference
+	# difference = np.chararray(difference)
+	# np.array(difference.tolist())
+	# print difference
+	# print pd.Series.str.len()
+	testData.loc[eachRow, 'difference'] = difference
+	# testData['differene'] = difference
+	print testData.difference
+
+
+
+
+
+
+
+# print(splitString)
 #   splitString = completeString.split("/")
 
 
